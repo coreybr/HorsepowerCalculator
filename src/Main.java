41cd36ded2d0be4c705856c13573effb2a648fd0
Corey Bradley <coreytbradley@gmail.com>
2016-10-21 15:13:34 -0400

    Set UI to use system-native look and feel

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
-	
+
 	private final static double version = 1.3;
-	
+
 	// Main method simply initializes GUI
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new UserInterface(version));
-    }
+	public static void main(String[] args) {
+
+		// Set to use native Look and Feel
+		try {
+			// Set System L&F
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
+
+		SwingUtilities.invokeLater(new UserInterface(version));
+	}
 }
